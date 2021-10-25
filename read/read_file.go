c1ba22dc3bfa45b7f9bbd2ef0b2e5659d0c5e272
李骞 <>
2021-10-25 10:53:53 +0800

    * update pkg

--- read/read_file.go
+++ read/read_file.go
@@ -1,8 +1,12 @@
 package read
 
 import (
+	"bufio"
 	"encoding/json"
+	"fmt"
+	"io/ioutil"
 	"os"
+	"strings"
 )
 
 const dataFile = "file/data.json"
@@ -22,4 +26,51 @@ func (f *Feed) ReadFeedByFile() ([]*Feed, error) {
 	var feedList []*Feed
 	err = json.NewDecoder(file).Decode(&feedList)
 	return feedList, err
+}
+
+//ReadGiftPackCode 读取文件转SQL
+func ReadGiftPackCode(file string, roomId int) string {
+	// 检测文件
+	if _, err := os.Stat(file); os.IsNotExist(err) {
+		panic(err)
+	}
+	// 小文件用 ioutil 读取
+	open, err := os.Open(file)
+	if err != nil {
+		panic(err)
+	}
+	defer open.Close()
+
+	b, err := ioutil.ReadAll(open)
+	if err != nil {
+		panic(err)
+	}
+	codes := strings.Split(string(b), "\n")
+	sql := "insert into `gift_pack` ( `room_id`,`activity_code` ) values "
+	for _, c := range codes {
+		sql += fmt.Sprintf("(%v, '%s'),", roomId, c)
+	}
+	sql = strings.TrimRight(sql, ",")
+	sql += ";"
+	return sql
+}
+
+// WriteFile 写入文件
+func WriteFile(fileName string, content string) error {
+	filePath := fmt.Sprintf("file/%s.sql", fileName)
+	file, err := os.OpenFile(filePath, os.O_WRONLY|os.O_CREATE, 0666)
+	if err != nil {
+		fmt.Println("文件打开失败", err)
+	}
+	//及时关闭file句柄
+	defer file.Close()
+
+	write := bufio.NewWriter(file)
+	_, _ = write.WriteString(content)
+	// 处理错误
+	err = write.Flush()
+	if err != nil {
+		return err
+	}
+	return nil
 }
--- read/read_file_test.go
+++ read/read_file_test.go
@@ -37,3 +37,28 @@ func TestFeed_ReadFeedByFile(t *testing.T) {
 		})
 	}
 }
+
+func TestReadGiftPackCode(t *testing.T) {
+	type args struct {
+		file   string
+		roomId int
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		{
+			name: "读取文件测试",
+			args: args{file: "file/code.txt", roomId: 1},
+			want: "insert into `gift_pack` ( `room_id`,`activity_code` ) values (1, 'UP1237LD7MF39121');",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := ReadGiftPackCode(tt.args.file, tt.args.roomId); got != tt.want {
+				t.Errorf("ReadGiftPackCode() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}

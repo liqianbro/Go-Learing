157706e51aa583c075b1477887e3d9ef93317986
李骞 <>
2021-10-26 13:44:00 +0800

    * add recover

--- main.go
+++ main.go
@@ -2,10 +2,24 @@ package main
 
 import (
 	"Go-Learing/app/router"
+	"fmt"
+	"runtime"
+
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
+	defer func() {
+		if err := recover(); err != nil {
+			for i := 3; ; i++ {
+				pc, file, line, ok := runtime.Caller(i)
+				if !ok {
+					break
+				}
+				fmt.Println(pc, file, line)
+			}
+		}
+	}()
 	// 设置为发布模式
 	gin.SetMode(gin.DebugMode)
 
